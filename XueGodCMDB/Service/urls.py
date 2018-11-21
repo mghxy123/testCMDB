936fdb51dc89eb4032dca05466458d8afe26612d
Forand <mghxy123@163.com>
2018-11-21 18:16:36 +0800

    第六章分页有无返回数据问题解决了

--- XueGodCMDB/Service/urls.py
+++ XueGodCMDB/Service/urls.py
@@ -10,7 +10,6 @@ from Service.views import *
 
 urlpatterns = [
     url(r'^api/',csrf_exempt(api.as_view())),
-    url(r'^serverData/',csrf_exempt(api.as_view())),
     url(r'^serverList/', serverList),  # as_view让我们定义的视图类像函数一样被调用
     url(r'^serverData/', serverData),  # as_view让我们定义的视图类像函数一样被调用
 ]
